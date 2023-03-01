ebfe15d45a13b6ad9bae96c523861f594c15aaf7
Kevin Li <kevinsl192k@gmail.com>
2023-02-28 22:57:59 -0800

    Successfully implemented detection of barcode scanner field

--- scripts/content.js
+++ scripts/content.js
@@ -1,2 +1,19 @@
-const barcodeField = document.getElementById("pageBeandisplayNameOfUserOrUserIndentifier")
- 
+window.addEventListener("load", (event) => {
+    var jsInitChecktimer = setInterval (checkForJS_Finish, 111);
+    function checkForJS_Finish () {
+        if (document.getElementById("pageBeanitemLoanParametersbarcode")) {
+            clearInterval (jsInitChecktimer);
+            input = document.getElementById("pageBeanitemLoanParametersbarcode")
+            input.addEventListener("keydown", interceptKey);
+        }
+    }
+});
+
+function interceptKey(e) {
+    var regex = /^CBK-\d{3}$/;
+    if (e.keyCode == "13" && regex.test(input.value)) {
+        e.preventDefault();
+        input.value = input.value.replace("-","")
+    }
+    
+}
