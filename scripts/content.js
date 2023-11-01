006d42ffbb8564e6360a28bce79cece940780078
Kevin Li <kevsl912@g.ucla.edu>
2023-11-01 13:41:30 -0700

    reformatted code

--- scripts/content.js
+++ scripts/content.js
@@ -1,16 +1,14 @@
 window.addEventListener("load", (event) => {
-    var jsInitChecktimer = setInterval (checkForJS_Finish, 100);
-    function checkForJS_Finish () {
-        if (document.getElementById("pageBeanitemLoanParametersbarcode")) {
-            input = document.getElementById("pageBeanitemLoanParametersbarcode");
-            input.addEventListener("keydown", interceptKey);
-        }
+    const input = document.getElementById("pageBeanitemLoanParametersbarcode");
+    if (input) {
+        input.addEventListener("keydown", interceptKey);
     }
 });
 
 function interceptKey(e) {
-    var regex = /^CBK-\d{3}$/;
-    if (e.keyCode == "13" && regex.test(input.value)) {
+    let regex = /^CBK-\d{3}$/;
+    let input = e.target;
+    if (e.key == "Enter" && regex.test(input.value)) {
         e.preventDefault();
         input.value = input.value.replace("-","");
         button = document.getElementById("fulfillment_dischargeok");
