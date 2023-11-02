80ed593a0f05cc5e5a83e22d862cc70f8aae31d3
Kevin Li <kevsl912@ucla.edu>
2023-11-01 20:52:47 -0700

    finished

--- scripts/content.js
+++ scripts/content.js
@@ -1,31 +1,18 @@
 let buttonIds;
 let barcodeFields;
-let pageTitle = "";
-let isObserving = false;
 
 // mutation function
 const mutationCallback = (mutationsList, observer) => {
   for (let mutation of mutationsList) {
     if (mutation.type === "childList") {
-      if (observer.customID == "barcodeObserver") {
-        for (const bf of barcodeFields) {
-          let targetElement = document.getElementById(bf);
-          if (targetElement) {
-            targetElement.addEventListener("keydown", interceptKey);
-            observer.disconnect();
-            return;
-          }
-        }
-      } else {
-        let pageTitleElem = document.querySelector(".pageTitle");
-        if (!pageTitleElem)
-        {
+      for (const bf of barcodeFields) {
+        let targetElement = document.getElementById(bf);
+        if (targetElement) {
+          targetElement.addEventListener("keydown", interceptKey);
+          const getMenuButton = document.querySelector('button[aria-label="Fullfillment"]');
+          getMenuButton.addEventListener('click', handleClick);
+          observer.disconnect();
           return;
-        }
-        if (
-          targetElement2.textContent != pageTitle
-        ) {
-          pageTitle = targetElement2.textContent;
         }
       }
     }
@@ -44,24 +31,25 @@ function interceptKey(e) {
       if (button) {
         button.click();
         observer.observe(document.body, config);
-        isObserving = true;
         return;
       }
     }
   }
 }
 
+//
+function handleClick(e) {
+  observer.observe(document.body, config);
+  e.target.removeEventListener('click', handleClick);
+}
+
 // observe
 const observer = new MutationObserver(mutationCallback);
-const observer2 = new MutationObserver(mutationCallback);
 const config = {
   childList: true,
   subtree: true,
 };
 observer.observe(document.body, config);
-observer.customID = "barcodeObserver";
-isObserving = true;
-observer2.observe(document.body, config);
 
 buttonIds = [
   "fulfillment_dischargeok",
